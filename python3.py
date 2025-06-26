9b4a00dcd52f00eba7c7db4c7347e78cf3c5ea94
Tareef-debugger <tareeftoufique@gmail.com>
2025-06-26 17:20:29 +0600

    Add files via upload

---
+++ python3.py
@@ -0,0 +1,11 @@
+x = input("Enter the value of x: ")
+y = input("Enter the value of y: ")
+
+
+temp = x
+x = y
+y = temp
+
+
+print("value of x after scrapping", x)
+print("value of y after scrapping", y)
